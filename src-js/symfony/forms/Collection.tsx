8236b6b339807db53c8480d1cf3eb7be9f3589f6
Norman Malessa <mail@norman-malessa.de>
2019-07-03 21:58:10 +0200

    fix event bubbling on collection button

--- src-js/symfony/forms/Collection.tsx
+++ src-js/symfony/forms/Collection.tsx
@@ -8,18 +8,18 @@ const htmlToNode = (html: string): Node => {
 const EnableCollection = (): void => {
     const elems = document.querySelectorAll('.js-symfony-collection');
     const $elems = [].slice.call(elems);
-    const addButton = htmlToNode('<a href="#" class="js-symfony-collection-add btn btn-secondary"><i class="fas fa-plus"></i></a>');
+    const addButton = htmlToNode('<button class="js-symfony-collection-add btn btn-secondary"><i class="fas fa-plus"></i></button>');
 
-    addButton.addEventListener('click', function(event: Event) {
+    addButton.addEventListener('click', function(event: any) { // @TODO figure out proper type
         event.preventDefault();
 
-        const clickedElem = event.target as HTMLElement;
-        const collectionContainer = clickedElem.parentNode.querySelectorAll('div')[0] as HTMLElement; //@TODO this does not seem to be very stable
+        const clickedElem = (event.target.nodeName === 'I') ? event.target.parentElement as HTMLElement : event.target as HTMLElement;
+        const collectionContainer = clickedElem.parentNode.querySelectorAll('div')[0] as HTMLElement;
         const labels = clickedElem.parentNode.querySelectorAll('.col-form-label');
         const lastLabel = (labels.length > 0) ? parseInt(labels[labels.length - 1].innerHTML) + 1 : 0;
         const newWidget = collectionContainer.dataset.prototype
-                                                        .replace(/__name__label__/g, lastLabel.toString())
-                                                        .replace(/__name__/g, lastLabel.toString());
+            .replace(/__name__label__/g, lastLabel.toString())
+            .replace(/__name__/g, lastLabel.toString());
 
         collectionContainer.appendChild(htmlToNode(newWidget));
     });
