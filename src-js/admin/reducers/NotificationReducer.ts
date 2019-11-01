26b0a7e2b512cfc955eced43f4a78a2f58378aa4
Norman Malessa <mail@norman-malessa.de>
2019-11-01 14:29:08 +0100

    add prettier

--- src-js/admin/reducers/NotificationReducer.ts
+++ src-js/admin/reducers/NotificationReducer.ts
@@ -1,16 +1,16 @@
-import { NotificationManager } from 'react-notifications';
+import { NotificationManager } from "react-notifications";
 
 const NotificationReducer = (state = [], action) => {
     switch (action.type) {
-        case 'ADD_SUCCESS_NOTIFICATION':
+        case "ADD_SUCCESS_NOTIFICATION":
             NotificationManager.success(action.text, null, 2000);
             break;
-        case 'ADD_ERROR_NOTIFICATION':
+        case "ADD_ERROR_NOTIFICATION":
             NotificationManager.error(action.text, null, 2000);
             break;
     }
 
     return state;
-}
+};
 
-export { NotificationReducer }
+export { NotificationReducer };
